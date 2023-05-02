42b7381e1da018e23f643c6b72e393fb976de990
abel mesfin <abelmesfin555@gmail.com>
2023-05-03 00:14:23 +0300

    output error fix

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route("/unauthorized/",
+@app_views.route("/api/v1/unauthorized",
                  strict_slashes=False)
 def unauthorized() -> str:
     '''Route unauthorized requests
@@ -36,7 +36,7 @@ def unauthorized() -> str:
     abort(401)
 
 
-@app_views.route("/forbidden/",
+@app_views.route("/api/v1/forbidden",
                  strict_slashes=False)
 def forbidden() -> str:
     '''Route forbidden requests
