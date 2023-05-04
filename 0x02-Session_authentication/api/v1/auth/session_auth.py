ef8728e8e87fbf6a1d2db085d6b1a5c600d08019
abel mesfin <abelmesfin555@gmail.com>
2023-05-04 13:05:14 +0300

    pycodestyle reauirement error fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,11 +14,12 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """
-        creates random session id and save the value to session dictionary
-        and return random session id
+        Creates a session ID for a user ID
         """
-        if user_id is None or isinstance(user_id, str):
+        if user_id is None or not isinstance(user_id, str):
             return None
+
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
+
         return session_id
