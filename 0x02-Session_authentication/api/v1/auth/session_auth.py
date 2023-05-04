ee746117ffc35e981cd9dbd5a1b9b73de6140e79
abel mesfin <abelmesfin555@gmail.com>
2023-05-04 12:51:36 +0300

    pycodestyle error fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,5 +4,10 @@ from .auth import Auth
 
 
 class SessionAuth(Auth):
-    """..."""
+    """
+    A subclass of Auth that provides session-based authentication.
+    It can be used in conjunction with a web framework to authenticate users
+    and restrict access to certain routes or resources.
+    
+    """
     pass
