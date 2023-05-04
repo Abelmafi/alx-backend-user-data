5f32557da59f736ad4e78d3afe8bd0cb963b0ed8
abel mesfin <abelmesfin555@gmail.com>
2023-05-04 13:00:08 +0300

    task2 inital commit

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Defination of class SessionAuth. """
 from .auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """
@@ -10,4 +10,11 @@ class SessionAuth(Auth):
     and restrict access to certain routes or resources.
     
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
